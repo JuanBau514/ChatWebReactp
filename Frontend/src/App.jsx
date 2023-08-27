fe8e66d9f0d3cfa9a824a5c2b9352e0e85526d4c
Juan Pablo Bautista <jbautistaclavijo@gmail.com>
2023-08-27 17:11:07 -0500

    todos los clientes reciben y envian mensajes de tipo broadcast falta hacer persistencia de datos mensajes privados y grupales

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,39 @@
 
 import io from "socket.io-client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const server = io("/");
+const server = io("/"); // conectamos con el servidor
 
 function app() {
 
-  const [mensajes, setMensajes] = useState([]); // creamos un estado para guardar los mensajes
+  const [mensaje, setMensaje] = useState(""); // creamos un estado para guardar un mensaje
+  const [mensajes, setMensajes] = useState([]); // creamos un estado para guardar los mensajes que se reciben
 
   const envioForm = (e) => { // esta funcion se ejecuta cuando se envia el formulario
     e.preventDefault(); // evitamos que se recargue la pagina
-    console.log(mensajes); // mostramos en consola el mensaje que se envio
+
+    const MensajeNuevo = {
+      datos: mensaje,
+      form: "Yo"
+    } 
+
+    setMensajes([...mensajes, MensajeNuevo]); // guardamos el mensaje en el estado mensajes
+    server.emit("mensaje", mensaje); // enviamos el evento "mensaje" al servidor con el mensaje que se envio
   } 
+
+  useEffect(() => { 
+
+    server.on("mensaje", AtendedorDeMensajes); // cuando se recibe un evento "mensaje" se ejecuta la funcion AtendedorDeMensajes
+
+    return() =>{
+      server.off('mensaje', AtendedorDeMensajes); // cuando se desmonta el componente desconectamos del servidor
+    } 
+
+  }, []); 
+
+
+const AtendedorDeMensajes = (mensaje) => 
+    setMensajes([...mensajes, mensaje]); // guardamos el mensaje en el estado mensajes    
 
     return (
         <div>
@@ -19,14 +41,23 @@ function app() {
           <form onSubmit={envioForm}>
 
             <input type="text" placeholder="Escribe tu mensaje ... "
-                onChange={(e) => setMensajes(e.target.value) } 
+                onChange={(e) => setMensaje(e.target.value) } 
             />
-
             <button type="submit">
               Enviar
             </button>
 
           </form>
+          <ul>
+              {
+                mensajes.map((mensajes, index) => (
+                  <li key={index}>
+                    {mensajes.form}:{mensajes.datos}
+                  </li>
+                ))
+              }
+
+          </ul>
         </div>
     )
 }
--- server-backend/index.js
+++ server-backend/index.js
@@ -7,7 +7,17 @@ const server = http.createServer(app);
 const io = new SocketServer(server); // creamos el servidor 
 
 io.on("connection", (socket) => {
-    console.log("Un cliente se ha conectado");
+
+    console.log("Un cliente se ha conectado", socket.id);
+
+    socket.on( 'mensaje' , (datos) => { 
+        console.log (datos)
+        socket.broadcast.emit('mensaje', { // enviamos (emitimos) el evento "mensaje" a todos los clientes conectados menos al que lo envio
+            datos,
+            form: socket.id.slice(4)
+        }) 
+    } )
+
 })
 
 server.listen(6900) // mi servidor esta escuchando en el puerto 6900

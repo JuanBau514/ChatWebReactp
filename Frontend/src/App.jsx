a380993564641cd4ae16e4c29994b245c3512e55
Juan Pablo Bautista <jbautistaclavijo@gmail.com>
2023-08-26 22:36:40 -0500

    el envio de informacion ya se registra en el navegador falta generar recursividad y enviar al servidor

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,32 @@
 
 import io from "socket.io-client";
+import { useState } from "react";
 
 const server = io("/");
 
 function app() {
+
+  const [mensajes, setMensajes] = useState([]); // creamos un estado para guardar los mensajes
+
+  const envioForm = (e) => { // esta funcion se ejecuta cuando se envia el formulario
+    e.preventDefault(); // evitamos que se recargue la pagina
+    console.log(mensajes); // mostramos en consola el mensaje que se envio
+  } 
+
     return (
         <div>
-            Hola Internet esto es un chat en tiempo real con opcion broadcast privado y grupal
+
+          <form onSubmit={envioForm}>
+
+            <input type="text" placeholder="Escribe tu mensaje ... "
+                onChange={(e) => setMensajes(e.target.value) } 
+            />
+
+            <button type="submit">
+              Enviar
+            </button>
+
+          </form>
         </div>
     )
 }
